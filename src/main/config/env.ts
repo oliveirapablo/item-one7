c41deeddcb53db62bf41360ca5ca491295e55614
Pablo Santana <pablo.d.o.s96@gmail.com>
2022-05-23 16:52:38 -0300

    feat: add start of app on main layer

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoUrl: process.env.MONGO_URL ? process.env.MONGO_URL : process.env.MONGO_URL_ONE7,
+  port: process.env.PORT ? process.env.PORT : process.env.PORT_ONE7_ITEM
+}
