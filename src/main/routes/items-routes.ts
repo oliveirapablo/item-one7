0cfbaa30469452729d564782dcb3b549850202a8
Pablo Santana <pablo.d.o.s96@gmail.com>
2022-05-23 16:44:25 -0300

    feat: add item router

---
+++ src/main/routes/items-routes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+import { makeAddItem } from '../factories/add-item'
+import { adaptRoute } from '../adapters/item'
+
+export default (router: Router): void => {
+  router.post('/item', adaptRoute(makeAddItem()))
+}
